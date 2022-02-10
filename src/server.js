9e8f2ddb1eb907dfbf8fb44e78425f99eade7d4b
AbubakarKang <90056797+AbubakarKang@users.noreply.github.com>
2022-02-10 18:11:12 +0800

    🔥 Removed unncessary file

--- src/server.js
+++
@@ -1,8 +0,0 @@
-//----------------------------\\ REQUIRES //----------------------------\\
-
-const mongoose = require("mongoose");
-require("dotenv").config();
-
-//-----------------------------\\ CONNECT //----------------------------\\
-
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
