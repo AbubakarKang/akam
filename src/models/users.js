193aa18c08229df0b8f54b5ffa5d1da7977a2933
AbubakarKang <kangabubakar33@gmail.com>
2022-11-23 00:06:11 +0800

    👔 Updated users table

--- src/models/users.js
+++ src/models/users.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
+	userID: Number, // Number of users + 1 *Must be unique
 	username: String,
 	password: String,
-	// email: String,
+	email: String,
+	isLoggedIn: Boolean, // Can only login from 1 device
 	creationDate: {
 		type: Date,
 		default: new Date(),
