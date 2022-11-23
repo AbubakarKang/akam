7c8816b905794da8ad66161fc17c701bab5f9a21
AbubakarKang <kangabubakar33@gmail.com>
2022-11-23 17:51:44 +0800

    ✨ Added custom error

--- src/js/register.js
+++ src/js/register.js
@@ -1,9 +1,14 @@
 //-----------------------------\\ ELEMENTS //-----------------------------\\
 
-const haveAccountAnchor = document.querySelector("[data-have-account-link]");
+const errorText = document.querySelector("[data-error-text]");
+const errorDisplay = document.querySelector("[data-error-blackbg]");
+const registerButton = document.querySelector("[data-register-btn]");
+const registerEmail = document.querySelector("[data-register-email]");
 const registerUsername = document.querySelector("[data-register-username]");
 const registerPassowrd = document.querySelector("[data-register-password]");
-const registerButton = document.querySelector("[data-register-btn]");
+const errorCloseButton = document.querySelector("[data-error-close-button]");
+const haveAccountAnchor = document.querySelector("[data-have-account-link]");
+const registerConfirmPassowrd = document.querySelector("[data-register-confirm-password]");
 
 //-----------------------------\\ FUNCTIONS //-----------------------------\\
 
@@ -13,10 +18,42 @@ haveAccountAnchor.addEventListener("click", () => {
 });
 
 registerButton.addEventListener("click", () => {
+	let email = registerEmail.value;
 	let username = registerUsername.value;
 	let password = registerPassowrd.value;
+	let passwordConfirm = registerConfirmPassowrd.value;
+
+	if (email === "" || username === "" || password === "" || passwordConfirm === "") {
+		return customAlert("Please fill all fields");
+	} else if (password !== passwordConfirm) {
+		return customAlert("Passwords do not match");
+	} else if (password.length < 6) {
+		return customAlert("Password must be at least 6 characters long");
+	} else if (password.length > 20) {
+		return customAlert("Password must be less than 20 characters long");
+	} else if (!email.includes("@") || !email.includes(".")) {
+		return customAlert("Please enter a valid email");
+	} else if (username.length < 6) {
+		return customAlert("Username must be at least 6 characters long");
+	} else if (username.length > 15) {
+		return customAlert("Username must be less than 20 characters long");
+	} else if (username.includes(" ")) {
+		return customAlert("Username cannot contain spaces");
+	} else if (username.includes("!") || username.includes("@") || username.includes("#") || username.includes("$") || username.includes("%") || username.includes("^") || username.includes("&") || username.includes("*") || username.includes("(") || username.includes(")") || username.includes("-") || username.includes("+") || username.includes("'") || username.includes('"')) {
+		return customAlert("Username can only contain letters, numbers and underscores");
+	}
+
 	ipc.send("registerUser", {
 		username,
 		password,
+		email,
 	});
 });
+
+const customAlert = error => {
+	errorText.innerText = error;
+	errorDisplay.style.display = "block";
+	errorCloseButton.addEventListener("click", () => {
+		errorDisplay.style.display = "none";
+	});
+};
