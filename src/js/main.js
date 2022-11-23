8f60f57476b3ffb29980ff5cc3c4053d630b18c9
AbubakarKang <kangabubakar33@gmail.com>
2022-11-23 20:44:01 +0800

    ✨ Added main page

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+//-----------------------------\\ ELEMENTS //-----------------------------\\
+
+const mainPage = document.querySelector("[data-main-page]");
+
+//-----------------------------\\ FUNCTIONS //-----------------------------\\
+
+ipc.on("userLoggedIn", () => {
+	mainPage.style.zIndex = "2";
+	mainPage.style.display = "block";
+	loginPage.style.display = "none";
+	registerPage.style.display = "none";
+});
