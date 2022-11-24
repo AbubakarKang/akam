8c463564ff53a741a84ad1a92c230c6c6fb9aaaa
AbubakarKang <kangabubakar33@gmail.com>
2022-11-24 14:51:23 +0800

    💡 Added necessary comments

--- src/index.js
+++ src/index.js
@@ -76,21 +76,27 @@ app.on("activate", () => {
 
 // Registering new user
 ipc.on("registerUser", (event, data) => {
+	// Define all data received from client
 	let receivedUsername = data.username;
 	let receivedPassword = data.password;
 	let receivedEmail = data.email;
+	// Store necessary login info to be used when account is registered
 	let loginInfo = [receivedUsername, receivedPassword];
 
+	// Encrypt user password and store it in database
 	let salt = bcryptjs.genSaltSync(10);
 	let hashedPassword = bcryptjs.hashSync(receivedPassword, salt);
 
 	User.find({ username: receivedUsername }, (error, data) => {
+		// Check if the username client entered is already in use
 		if (typeof data[0] !== "undefined") {
 			let existingUsername = data[0].username;
 			return event.sender.send("userExists", existingUsername);
 		} else {
 			User.find({}, (error, data) => {
+				// newUserID is the number of registered accounts + 1
 				let newUserID = data.length + 1;
+				// Create an account with the necessary information
 				let newUser = new User({
 					username: receivedUsername,
 					password: hashedPassword,
@@ -98,6 +104,7 @@ ipc.on("registerUser", (event, data) => {
 					userID: newUserID,
 				});
 				newUser.save();
+				// Display notification that account is registered
 				event.sender.send("userRegistered", loginInfo);
 			});
 		}
@@ -106,9 +113,11 @@ ipc.on("registerUser", (event, data) => {
 
 // Login user
 ipc.on("loginUser", (event, data) => {
+	// Define all data received from client
 	let receivedUsername = data.username;
 	let receivedPassword = data.password;
 	User.find({ username: receivedUsername }, (error, data) => {
+		// Check if the username client entered exists in database or not
 		if (typeof data[0] === "undefined") {
 			return event.sender.send("userNotFound");
 		} else {
@@ -116,19 +125,23 @@ ipc.on("loginUser", (event, data) => {
 			let databasePassword = foundUser.password;
 
 			bcryptjs.compare(receivedPassword, databasePassword, (error, res) => {
+				// Check if the password client entered matches the password stored in database
 				if (res) {
 					User.find({ username: receivedUsername }, (error, data) => {
 						let isUserLoggedIn = data[0].isLoggedIn;
+						// Check if the account is already in use on another window or device
 						if (isUserLoggedIn) {
 							return event.sender.send("alreadyLoggedIn");
 						} else {
 							User.findOneAndUpdate({ username: receivedUsername }, { isLoggedIn: true }, () => {
+								// Log the user in
 								accountInUse = receivedUsername;
 								event.sender.send("userLoggedIn");
 							});
 						}
 					});
 				} else {
+					// Inform user of wrong password entered
 					event.sender.send("wrongPassword");
 				}
 			});
