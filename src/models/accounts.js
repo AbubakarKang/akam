58b84aec0a2d1f90b4e3e6705389578203a3c151
AbubakarKang <kangabubakar33@gmail.com>
2022-11-25 16:38:28 +0800

    🧱 Added account schema

---
+++ src/models/accounts.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const accountSchema = new mongoose.Schema({
+	user: String,
+	accountName: String,
+	accountPassword: String,
+	accountEmail: String,
+	accountInfo: String,
+	accountImage: String,
+	creationDate: {
+		type: Date,
+		default: new Date(),
+	},
+});
+
+module.exports = mongoose.model("Account", accountSchema);
